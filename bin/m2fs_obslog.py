33cb2ba150bd1b88f6c02e8be51047f71308938e
Jeb Bailey <baileyji@umich.edu>
2013-12-15 12:35:23 -0500

    obslog enhancements

--- bin/m2fs_obslog.py
+++ bin/m2fs_obslog.py
@@ -2,34 +2,54 @@
 from m2fs.obs import summarize
 import os
 import sys
+import argparse
+
+def parse_cl():
+    parser = argparse.ArgumentParser(description='Observing Log generator',
+                                     add_help=True)
+    parser.add_argument('-d','--dir', dest='dir',
+                        action='store', required=False, type=str,
+                        help='source dir for files',default='./')
+    return parser.parse_args()
+
+#"<html><body>\n"+'\n'.join(rows)+"</html></body>\n"
+#header_row="<b>"+column_fmtstr.format(column_names)+"</b>"
+#rows=[]
+#for i, r in enumerate(recs):
+#    if i % 40 ==0:
+#        rows.append(header_row)
+#    rows.append(r)
+
 
 def filter_files(files):
     """Only show merged frame or first quadrant (if merged not available)"""
     out=[]
     for f in files:
+        fname=os.path.basename(f)
         try:
-            if 'c' not in f or ('c1' in f and f[0:-7]+'.fits' not in files):
+            if ('c' not in fname or
+                ('c1' in fname and f[0:-7]+'.fits' not in files and
+                f[0:-7]+'.fits.gz' not in files)):
                 out.append(f)
         except IndexError:
                 out.append(f)
     return out
 
 def hjobs(header):
-    return (header['FILTER']=='HotJupiter' or
-            (int(header['FILENAME'][1:-2]) in range(106,116)))
+    return True
+#    return (header['FILTER']=='HotJupiter' or
+#            (int(header['FILENAME'][1:-2]) in range(106,116)))
 
 if __name__ =='__main__':
-    if len(sys.argv) >1 :
-        dir = sys.argv[1]
-    else:
-        dir ='./'
+    args=parse_cl()
 
     files = [os.path.join(dirpath, f)
-             for dirpath, dirnames, files in os.walk(dir)
-             for f in files if f.endswith('.fits')]
-             
+             for dirpath, dirnames, files in os.walk(args.dir)
+             for f in files if f.endswith('.fits') or f.endswith('.fits.gz')]
+
     files=filter_files(files)
+
     summarize.run(files,
-                  prepend=['NIGHT', 'RA','DEC'],
-                  append=['FF-THNE','FF-THAR','FF-QRTZ'],
-                  filterfunc=hjobs)
+              prepend=['NIGHT', 'RA','DEC'],
+              append=['FF-THNE','FF-THAR','FF-QRTZ'],
+              filterfunc=hjobs)
--- m2fs/obs/summarize.py
+++ m2fs/obs/summarize.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 records=[]
 keys=[]
 default_keys=['FILENAME','OBJECT', 'EXPTIME', 'BINNING', 'SLIDE',
-          'HI-AZIM', 'HI-ELEV', 'FOCUS', 'FILTER']
+          'HI-AZIM', 'HI-ELEV','LO-ELEV', 'FOCUS', 'FILTER']
 keys=[]
 @app.route('/')
 def summary_table():
@@ -22,9 +22,10 @@ def run(files, userkeys=None, prepend=[], append=[], filterfunc=lambda x:True):
     else:
         keys=userkeys
     keys=prepend+keys+append
+    print "Summarizing {} files.".format(len(files))
     for f in files:
         header=fits.getheader(f)
         if filterfunc(header):
             records.append([header[k] for k in keys])
-    app.debug=True
+    #app.debug=True
     app.run()
