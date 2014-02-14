6fe290af32336e60f2e3f499b4d5d994fd9870db
Jeb Bailey <baileyji@umich.edu>
2014-02-14 15:59:31 -0300

    first attempt at a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='M2FS',
+      version='0.1',
+      description='M2FS reduction utilities',
+      author='Jeb Bailey',
+      author_email='baileyji@umich.edu',
+      url='',
+      packages=['m2fs'],
+      )
