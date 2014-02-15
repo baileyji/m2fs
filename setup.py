820c1b428cc32878661af636d61f67c8556a71b7
Jeb Bailey <baileyji@umich.edu>
2014-02-15 00:36:42 -0300

    pip install now works

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 from distutils.core import setup
 
 setup(name='M2FS',
-      version='0.1',
+      version='0.2',
       description='M2FS reduction utilities',
       author='Jeb Bailey',
       author_email='baileyji@umich.edu',
       url='',
-      packages=['m2fs'],
+      packages=['m2fs','m2fs.obs','m2fs.plate'],
+      scripts=['bin/m2fs_merge.py',
+               'bin/m2fs_obslog.py',
+               'bin/m2fs_platesummary.py',
+               'bin/m2fs_scatter.py',
+               'bin/m2fs_stack.py'],
+      install_requires=['numpy']
       )
