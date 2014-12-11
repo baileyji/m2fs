64f9590648828e88b6bb464d6876f62d9a930f4c
Jeb Bailey <baileyji@umich.edu>
2014-12-11 20:10:14 -0300

    dependency links are deprecated.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 from distutils.core import setup
 
 
-jbastro_dep_link='https://github.com/baileyji/jbastro/tarball/v0.3#egg=jbastro-0.3'
-
 setup(name='M2FS',
       version='0.4',
       description='M2FS reduction utilities',
@@ -18,6 +16,5 @@ setup(name='M2FS',
                'bin/m2fs_scatter.py',
                'bin/m2fs_stack.py',
                'bin/m2fs_focus.py'],
-      install_requires=['numpy','jbastro>=0.3'],
-      dependency_links = [jbastro_dep_link]
+      install_requires=['numpy>=1.8.0']
       )
